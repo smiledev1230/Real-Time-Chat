508072a0b35e88fca38e63033a1b0e89fa6aa806
Web <30370727+smile-monkey@users.noreply.github.com>
2018-04-12 16:47:31 +0700

    initial commit

---
+++ MyChat/ChatViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyChat
+//
+//  Created by Develop on 4/12/18.
+//  Copyright © 2018 Develop. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
